27496ac0e729ee78bd7c9142b6a290b2ebed153b
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-08-26 17:46:11 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require("pg");
+const app = pg();
 const cors = require("cors");
 const pool = require("./db");
 
 app.use(cors());
-app.use(express.json());
+app.use(pg.json());
 
 app.post("/employees", async (req, res) => {
     try {
