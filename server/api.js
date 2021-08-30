e33cf730129c411e87920d2013068889023030c7
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-08-30 17:29:17 +0530

    Update api.js

--- server/api.js
+++ server/api.js
@@ -20,3 +20,46 @@ app.get('/users', (req, res)=>{
     });
     client.end;
 })
+
+app.post('/add', (req, res)=> {
+    const user = req.body;
+    let insertQuery = `insert into users(id, firstname, lastname, location) 
+                       values(${user.id}, '${user.firstname}', '${user.lastname}', '${user.location}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
+
+app.put('/update/:id', (req, res)=> {
+    let user = req.body;
+    let updateQuery = `update users
+                       set firstname = '${user.firstname}',
+                       lastname = '${user.lastname}',
+                       location = '${user.location}'
+                       where id = ${user.id}`
+
+    client.query(updateQuery, (err, result)=>{
+        if(!err){
+            res.send('Update was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
+
+app.delete('/delete/:id', (req, res)=> {
+    let insertQuery = `delete from users where id=${req.params.id}`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Deletion was successful')
+        }
+        else{ console.log(err.message) }
+    })
+    client.end;
+})
