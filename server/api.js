e9d90173a80b9c8c1861e22f545e4cb7b8d590c0
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-08-30 16:15:42 +0530

    Update api.js

--- server/api.js
+++ server/api.js
@@ -1,6 +1,10 @@
 const client = require('./db.js')
 const express = require('express');
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(5000, ()=>{
     console.log("Sever is now listening at port 5000");
