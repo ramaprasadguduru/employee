7d3975fb63d83a51e70ea46f4a1f840704436ff5
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-08-31 07:45:42 +0530

    Update Listemployees.js

--- client/src/components/Listemployees.js
+++ client/src/components/Listemployees.js
@@ -8,7 +8,7 @@ const Listemployees = () => {
   const getemployees = async() => {
     try {
 
-      const response = await fetch("http://100.26.106.102:4000/testAPI");
+      const response = await fetch("http://172.31.87.152:5000/employee");
       console.log(response);
       const jsonData = await response.json();
 
@@ -21,7 +21,7 @@ const Listemployees = () => {
 
   const deleteemployee = async (id) => {
     try {
-      const deleteemployee = await fetch(`http://100.26.106.102:4000/employees/${id}`, {
+      const deleteemployee = await fetch(`http://172.31.87.152:5000/employees/${id}`, {
         method: "DELETE"
       });
 
