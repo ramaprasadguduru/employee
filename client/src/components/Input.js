617c3d4f493edaf1c6916c0f473b6b1f6ee9e190
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-08-31 13:27:52 +0530

    Update Input.js

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -9,7 +9,7 @@ const Input = () => {
       try {
           
           const body = { name };
-          await fetch("http://3.87.141.235:5000/employees", {
+          await fetch("http://3.87.141.235:5000/add", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(body)
