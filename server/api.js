1acc16c158f484231169a5ef5ded2c4b5ba7850d
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-08-31 07:39:35 +0530

    Update api.js

--- server/api.js
+++ server/api.js
@@ -11,5 +11,13 @@ app.listen(5000, ()=>{
 })
 
 client.connect();
+app.get('/users', (req, res)=>{
+    client.query(`Select * from employee`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
 
 
