3b22bc3d98abc39ad720f27813debb48cea79891
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-08-31 10:45:24 +0530

    Update Listemployees.js

--- client/src/components/Listemployees.js
+++ client/src/components/Listemployees.js
@@ -8,7 +8,7 @@ const Listemployees = () => {
   const getemployees = async() => {
     try {
 
-      const response = await fetch("http://3.87.141.235:5000/users");
+      const response = await fetch("http://3.87.141.235:5000/employee");
       console.log(response);
       const jsonData = await response.json();
 
