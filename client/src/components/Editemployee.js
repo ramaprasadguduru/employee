c5c8a1d30b4cb143a47a8aae0d6cfe5245053ce6
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-10-01 12:09:03 +0530

    Update Editemployee.js

--- client/src/components/Editemployee.js
+++ client/src/components/Editemployee.js
@@ -8,7 +8,7 @@ const Editemployee = ({employee}) => {
     e.preventDefault();
     try {
       const body = { name };
-      const response = await fetch(`http://172.31.83.168:5000//update/${employee.id}`, {
+      const response = await fetch(`http://3.92.212.1:5000//update/${employee.id}`, {
         method: "PUT",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(body)
